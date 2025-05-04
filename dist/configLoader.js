ff92dfb28cbaec24742ddb8e86956ac834bf5028
Eric Abouaf <eric.abouaf@gmail.com>
2025-05-04 10:47:02 +0200

    Build

--- dist/configLoader.js
+++ dist/configLoader.js
@@ -70,10 +70,10 @@ class ConfigLoader {
             };
         }
         // Only construct llm if provider or model is present
-        if (opts.llmProviderType || opts.llmModel || opts.llmTemperature || opts.llmMaxTokens || opts.llmProviderOption) {
+        if (opts.llmProvider || opts.llmModel || opts.llmTemperature || opts.llmMaxTokens || opts.llmProviderOption) {
             const llm = {};
-            if (opts.llmProviderType) {
-                llm.provider = { type: opts.llmProviderType };
+            if (opts.llmProvider) {
+                llm.provider = { type: opts.llmProvider };
                 if (opts.llmProviderOption) {
                     llm.provider.options = {};
                     for (const kv of opts.llmProviderOption) {
